82d0f83c555a99c1183f12fa2826f9d4ad26e0e5
AgustinaGonzalezAG <139406447+AgustinaGonzalezAG@users.noreply.github.com>
2023-11-07 05:12:50 +0000

    [contador funcionando]

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,32 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom";
+import fontAwesomeLibrary from './fontAwesomeConfig';
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import SecondCounter from "./SecondCounter";
 
-//import your own components
-import Home from "./component/home.jsx";
+let seconds = 0
+setInterval(() => {
+    // calculo los digitos de cada variable
+    let unidades = seconds % 10;
+    let decenas = Math.floor((seconds / 10) % 10);
+    let centenas = Math.floor((seconds / 100) % 10);
+    let unidadesMil = Math.floor((seconds / 1000) % 10);
+    let decenasMil = Math.floor((seconds / 10000) % 10);
+    let centenasMil = Math.floor((seconds / 100000) % 10);
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+    ReactDOM.render(<SecondCounter
+        tiempo={seconds}
+        unidades={unidades}
+        decenas={decenas}
+        centenas={centenas}
+        unidadesMil={unidadesMil}
+        decenasMil={decenasMil}
+        centenasMil={centenasMil}
+         />, document.querySelector("#app"));
+    seconds++
+}, 1000)
+
+
